38f5527c103010ad4c0a252d8ad2b9921cc0ba99
PEDRO LINS <pedro.hcl29@gmail.com>
2024-05-28 16:25:19 -0300

    first commit

---
+++ src/candidatura/ParametrosInvalidosException.java
@@ -0,0 +1,12 @@
+package candidatura;
+
+
+import java.util.Scanner;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
+
+
